7c5ec33eaa232c1917d48e18772a7264044c9e65
ALina Ulyanova <ulyanova.alina1997@gmail.com>
2020-04-23 17:13:44 +0300

    mdkd

--- src/components/AudioPlayer/audio.js
+++ src/components/AudioPlayer/audio.js
@@ -25,16 +25,20 @@ const audio = (WrappedComponent) => {
 
         // const [player, setPlayer] = useState(null);
         let sources = [];
-        let isPlaying = false;
+        // let isPlaying = false;
         let startTime = 0;
-        let lastChunkOffset = 0;
+        // let lastChunkOffset = 0;
         let audioBuffer = null;
-        const bufferSize = 6;
+        // const bufferSize = 6;
         // let playWhileLoadingDuration = 0;
         // let source = null;
         let chunks = [];
         let nextTime = 0.01;
         let activeSource = null;
+
+        let fileSize_ = null;
+        let fileDuration = null;    
+        let chunkSize_ = null;
 
         const appendBuffer = (buffer1, buffer2) => {
             const numberOfChannels = Math.min(buffer1.numberOfChannels, buffer2.numberOfChannels);
@@ -82,6 +86,46 @@ const audio = (WrappedComponent) => {
             }
         }, 500);
 
+        const playByTimepoint = (timepoint) => {
+                // const share = chunkSize / fileSize;
+                // console.log(share);
+                //
+                // const durationOfChunk = share * fileDuration;
+                // console.log(durationOfChunk);
+                //
+                // const chunkNum = Math.floor(timepoint / durationOfChunk);
+                // console.log('chunkNum', chunkNum);
+
+            console.log('playByTimepoint');
+
+            const numOfChunks = fileSize_ / chunkSize_;
+            console.log('chunkSize_', chunkSize_);
+
+            const chunkNum = Math.floor((timepoint * numOfChunks) / 100);
+            console.log('chunkNum', chunkNum);
+
+            socket.emit('play', {
+                // chunkNumber: chunkNum,
+                startSize: chunkNum * chunkSize_
+            });
+            // // //
+            // // //     // let chunkNumber = 0;
+            // // //     // let currDuration = durationOfChunk;
+            // // //     // let currSize = chunkSize;
+            // // //     // while (currDuration !== fileDuration && timePoint > currDuration) {
+            // // //     //     currDuration += durationOfChunk;
+            // // //     //     currSize += chunkSize;
+            // // //     //     chunkNumber++;
+            // // //     // }
+            // // //     //
+            // // //     // console.log('num ' + chunkNumber);
+            // // //     // socket.emit('fromChunk', {
+            // // //     //     chunkNumber: chunkNumber-1,
+            // // //     //     currDuration: currDuration-durationOfChunk,
+            // // //     //     currSize: currSize-chunkSize
+            // // //     // });
+        };
+
         const onPlayBtnClick = () => {
             // useCallback(() => {
             dispatch(onPlay());
@@ -89,6 +133,25 @@ const audio = (WrappedComponent) => {
             console.log(`Playing ${playerData.isPlaying}`);
 
             socket.emit('track', (e) => {
+                // socket.on('metadata', ({fileSize, chunkSize}) => {
+                //     this.fileSize = fileSize;
+                //     // this.fileDuration = fileDuration;
+                //     this.chunkSize = chunkSize;
+                // });
+                //
+                // playByTimepoint(0);
+            });
+
+            socket.on('metadata', ({fileSize, chunkSize}) => {
+                console.log('metadata');
+
+                fileSize_ = fileSize;
+                // this.fileDuration = fileDuration;
+                chunkSize_ = chunkSize;
+
+                console.log('chunkSize_', chunkSize_);
+
+                playByTimepoint(0);
             });
 
             nextTime = 0;
